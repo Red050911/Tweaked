59fa2312ea38a080c9574cd3ba494e48fed1051a
RedstoneBoy0509 <jacob.rutledge0509@gmail.com>
2022-02-28 20:34:09 -0600

    Initial commit

---
+++ src/main/java/io/github/red050911/plugin/tweaked/Tweaked.java
@@ -0,0 +1,16 @@
+package io.github.red050911.plugin.tweaked;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tweaked extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
